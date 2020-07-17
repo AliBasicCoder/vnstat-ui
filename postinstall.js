0e54aa34ceb4f297a286edfc504dcbb0f285eb8e
alianmad <aliahmedreda34@gmail.com>
2020-07-17 04:45:53 +0200

    ..

--- postinstall.js
+++ postinstall.js
@@ -3,7 +3,7 @@ const homedir = require("os").homedir();
 
 const vnstatDir = new Dir(homedir, "vnstat-ui");
 const configFile = new File(homedir, "vnstat-ui/vnstat-ui-data.json");
-configFile.write(new File(__dirname, "config.json").json());
 if (vnstatDir.exits()) process.exit();
+configFile.write(new File(__dirname, "config.json").json());
 vnstatDir.create();
 vnstatDir.createDir("themes");
